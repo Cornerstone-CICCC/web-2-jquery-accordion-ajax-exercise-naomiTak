c9d06e6730ba1fcb89e99b0824a39bbfc5ef5a5c
Nao Gobaru <take57bamboo@gmail.com>
2024-07-31 16:57:21 -0700

    add solution

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 $(function() {
   // your code here
+  const header = $(".accordion-header");
+  const btn = $("button");
+
+  header.on("click", function(){
+    $(this).next().slideToggle()
+  })
+
+  btn.on("click", function(){
+    $.ajax({
+      url: "https://dummyjson.com/todos",
+      type: "GET",
+      success: function(response){
+        console.log("the response", response.todos)
+        const todos = response.todos;
+        $.each(todos, function(index, value){
+          console.log(index+" : "+value.todo)
+          $("ul").append(`<li>${value.todo}</li>`)
+        })
+      },
+      error: function(error){
+        console.error("Error message", error)
+      }
+    })
+  })
 })
